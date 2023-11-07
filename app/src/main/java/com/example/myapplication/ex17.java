1b0f637e5654a7686a6ce425006b8b450ad58f9c
smokiit <smo.nicek08@yahoo.com>
2023-11-07 15:49:30 +0900

    typed ex17 and created ex17.java

---
+++ app/src/main/java/com/example/myapplication/ex17.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ex17 extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
